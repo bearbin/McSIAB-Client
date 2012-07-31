84c0dec6d5f1e482f33d3a6f3ff060e59b2663ac
Alexander Harkness <bearbin@gmail.com>
2012-07-31 09:12:35 +0100

    Added shared code directory and moved nukedir there.

---
+++ shared/nukedir.py
@@ -0,0 +1,13 @@
+import os
+
+def nukedir(dir):
+	if dir[-1] == os.sep: dir = dir[:-1]
+	files = os.listdir(dir)
+	for file in files:
+		if file in ['.', '..']: continue
+		path = dir + os.sep + file
+		if os.path.isdir(path):
+			nukedir(path)
+		else:
+			os.unlink(path)
+	os.rmdir(dir)
