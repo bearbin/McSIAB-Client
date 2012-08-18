3acf60dcda19751a873e2f7ea0ec6fe21d727fa9
Alexander Harkness <bearbin@gmail.com>
2012-08-18 15:08:40 +0100

    Added more comments...

--- fetchserverlist.py
+++ fetchserverlist.py
@@ -2,6 +2,12 @@ import urllib
 import yaml
 
 def fetch_serverlist(serverURL):
+    #
+    # Fetches the information YAML file, parses it and returns a python opject with the data inside.
+    #
+    # serverURL : The URL of the server hosting the server list.
+    #
+
     yamlFileOnServer = urllib.urlopen(serverURL+"/serverzips/list.yml")
     parsedFile = yaml.load(yamlFileOnServer)
     yamlFileOnServer.close()
