4bfc705d0d5a3910573d3d42079baaaeb2fd1912
Alexander Harkness <bearbin@gmail.com>
2012-08-18 15:15:40 +0100

    Added more comments

--- downloadzip.py
+++ downloadzip.py
@@ -1,6 +1,14 @@
 import urllib
 
 def download_zip(url, saveLocation):
+    
+    #
+    # Downloads a zip fron the specified URL and saves it to the specified location.
+    #
+    # url          : The URL of the zip file to be downloaded.
+    # saveLocation : Where the downloaded zip is to be saved.
+    #
+
     print "Zip Download Started."
     zipToDownload = urllib.urlopen(url)
     localFile = open(saveLocation, 'wb')
